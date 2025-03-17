598877703105fe1b4fee647c19b42907aa8a9bef
Chriss Furenes <chrissof@uia.no>
2025-03-17 04:17:28 +0100

    adding

---
+++ fast-flare/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://ChrissFurenes.github.io',
+    base: 'CTF_Writeups',
+});
