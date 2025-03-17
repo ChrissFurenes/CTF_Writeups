d0dbed9c8e664ca63fc8bbaa7a076be8b8c04f6c
Chriss Furenes <chrissolfu@gmail.com>
2025-03-17 09:41:23 +0100

    new path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://chrissfurenes.github.io',
+    base: 'CTF_Writeups',
 });
